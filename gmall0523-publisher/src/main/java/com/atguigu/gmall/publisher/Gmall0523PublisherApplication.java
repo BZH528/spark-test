f78b6b35c42c22e963e1aa5ad4b2fd51ca99e5b8
bizhihao <1261819392@qq.com>
2021-04-29 19:14:26 +0800

    gmall0523-publish初始化项目

---
+++ gmall0523-publisher/src/main/java/com/atguigu/gmall/publisher/Gmall0523PublisherApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0523PublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0523PublisherApplication.class, args);
+    }
+
+}
