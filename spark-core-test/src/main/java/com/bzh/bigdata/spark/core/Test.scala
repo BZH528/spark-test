6300f14973778942b60c21a60289615181ffa2a8
bizhihao <1261819392@qq.com>
2021-06-02 18:10:28 +0800

    添加测试

--- spark-core-test/src/main/java/com/bzh/bigdata/spark/core/Test.scala
+++ spark-core-test/src/main/java/com/bzh/bigdata/spark/core/Test.scala
@@ -1,9 +1,20 @@
 package com.bzh.bigdata.spark.core
 
+import scala.collection.mutable
+
 object Test {
 
   def main(args: Array[String]): Unit = {
 
     println("hello world")
+
+    var userMap = mutable.Map[String,String]()
+    userMap.put("1","aa")
+    userMap.put("2","bb")
+    userMap.put("3","cc")
+
+    val value: String = userMap.get("2").get
+
+    println(value)
   }
 }
