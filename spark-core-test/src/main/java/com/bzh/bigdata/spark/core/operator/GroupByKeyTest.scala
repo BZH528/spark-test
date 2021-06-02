8e1bf462d676bdc3050fd59793f962f129cda189
bizhihao <1261819392@qq.com>
2021-06-02 18:10:11 +0800

    添加测试

--- spark-core-test/src/main/java/com/bzh/bigdata/spark/core/operator/GroupByKeyTest.scala
+++ spark-core-test/src/main/java/com/bzh/bigdata/spark/core/operator/GroupByKeyTest.scala
@@ -12,12 +12,17 @@ object GroupByKeyTest {
 
     val sc: SparkContext = new SparkContext(sparkConf)
 
-    val numRDD = sc.makeRDD(List(("a",1), ("a",2), ("b",3), ("a",4), ("b",5), ("a",6)), 2)
+    val numRDD = sc.makeRDD(List(("a",1), ("a",2), ("b",3), ("a",4), ("b",5), ("a",6)))
 
-    val outputRDD: RDD[(String, Iterable[Int])] = numRDD.groupByKey()
+//    val outputRDD: RDD[(String, Iterable[Int])] = numRDD.groupByKey()
+
+    val reduceByKeyRDD: RDD[(String, Int)] = numRDD.reduceByKey((a,b) => b)
 
 
-    outputRDD.collect().foreach(println)
+//    outputRDD.collect().foreach(println)
+    reduceByKeyRDD.collect().foreach(println)
+
+
 
     sc.stop()
 
